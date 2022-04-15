bb626601b76460042f17484110e81dfa9fb70aa2
Mit <mitpancholi1012@gmail.com>
2022-04-15 11:33:02 +0530

    publish changes

--- dist/cjs/context/quiz.d.ts
+++ dist/cjs/context/quiz.d.ts
@@ -1,7 +1,7 @@
 import React from 'react';
 import { QuizProviderProps, QuizState, ReportState, SimplifiedQuestion } from '../utility/interfaces';
 import { QuizActions } from './actions/quiz';
-declare const QuizProvider: React.FC<QuizProviderProps>;
+declare const QuizProvider: React.FC<QuizProviderProps & React.ReactNode>;
 export declare const useQuiz: () => {
     getQuestion: (questionId: number) => SimplifiedQuestion;
     getSavedAnswer: (questionId: number) => number | number[] | "";
